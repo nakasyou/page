5be494b4e98d679e52f0ae67c76fc6647de3cd7f
nakasyou <nakasyou1103@gmail.com>
2023-03-07 18:56:11 +0900

    fix

---
+++ components/head.vue
@@ -0,0 +1,14 @@
+<template>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="stylesheet" href="global-style.css">
+    </head>
+</template>
+<script>
+
+export default {
+  name: 'NuxtTutorial'
+}
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,78 @@
 <template>
-  <Tutorial/>
+  <!DOCTYPE html>
+  <html lang="ja">
+
+  <head>
+
+    <Head />
+    <link rel="stylesheet" href="style.css">
+    <meta name="description" content="Liberlunaのページてす。">
+    <meta name="robots" content="index, follow">
+    <title>Liberluna - Official</title>
+  </head>
+
+  <body>
+    <header id="header" hidden><Header/></header>
+    <div id="title-zone">
+      <div id="main-title">
+        <img src="img/liberluna.svg" alt="Liberluna" style="max-width:100%" id="light-logo">
+        <img src="img/liberluna-white.svg" alt="Liberluna" style="max-width:100%" id="dark-logo">
+      </div>
+      <div id="scroll-to">
+        <img src="img/scroll.svg">
+      </div>
+    </div>
+    <div id="main-let">
+      <br><br>
+      <div>
+        <h2>What is "Liberluna"?</h2>
+        <div id="about">
+          Liberlunaは、インターネット上で集まった、ハッカー/プログラマ集団です。LINEオープンチャットや、Elementで活動をしています。
+        </div>
+      </div>
+      <div>
+        <h2>What does "Liberluna" do?</h2>
+        <div id="about">
+          Liberlunaは、掲示板・ハッキングテストを制作しています。気になる方は、LINEオープンチャットまでお越しください。歓迎します。<br>
+          また、LINEオープンチャットでは、セキュリティやコンピュータに関する雑談もしています。
+        </div>
+        <center>
+          <a
+            href="https://line.me/ti/g2/beNFT8zr5MEL_Um5xmSBZthZwfIuZcQu8-bYMA?utm_source=invitation&utm_medium=link_copy&utm_campaign=default">LINEオープンチャット</a>
+        </center>
+      </div>
+      <div>
+        <h2>Services</h2>
+        <div id="about">
+          <h3>開発中のサービス</h3>
+          <div style="display:flex;justify-content: center;">
+            <div style="text-align: center;">
+              <img src="img/keiji-icon.svg" style="width:10rem">
+              <br>
+              <b>掲示板</b>
+              <br>
+              <b>掲示板です。</b>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2 style="padding-left: 30px;">LINKS</h2>
+        <div id="about">
+          <div style="text-align: center;">
+            <a href="members">
+              <img src="img/users.svg" style="width:10rem;">
+            </a>
+            <br>
+            <h3>Members</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="script.js"></script>
+  </body>
+
+  </html>
 </template>
 
 <script lang="ts">
