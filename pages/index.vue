f5c6f40462035572cbcc94b8d5bbfe3a68759807
nakasyou <nakasyou1103@gmail.com>
2023-03-09 11:58:33 +0000

    Revert "Update index.vue"
    This reverts commit 780b42c2bc03a697ea19df03be743a2741765f3d.

--- pages/index.vue
+++ pages/index.vue
@@ -6,20 +6,20 @@
         
       </head>
       <body>
-      <p>a</p>
+        <LiberlunaHeader/>
       </body>
     </html>
   </client-only>
 </template>
 
 <script lang="ts">
-//import LiberlunaHeader from '~/components/liberluna_header.vue'
+import LiberlunaHeader from '~/components/liberluna_header.vue'
 import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
   components: {
-    //LiberlunaHeader
+    LiberlunaHeader
   },
 })
 </script>
