7db85526da06df4818dd42f0de49b5ad44218024
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-11 14:42:44 +0900

    Add style

--- components/liberluna_footer.vue
+++ components/liberluna_footer.vue
@@ -1,10 +1,15 @@
 <template>
   <footer>
-    <div>
+    <div class="footer-main">
       &copy;2023&nbsp;Liberluna
     </div>
   </footer>
 </template>
+<style>
+  .footer-main{
+    width:100%
+  }
+</style>
 <script>
 export default {
   name: "LiberlunaFooter"
