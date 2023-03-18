2e3d9f71ac4cac2800f85adc73c16577b534c3ce
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-18 17:02:25 +0900

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,6 @@ module.exports = {
     "comma-dangle": ["off","never"],
     "comma-spacing": ["off"],
     "eol-last": ["off"],
-    "space-before-function-paren": [{"anonymous": 2, "named": 0, "asyncArrow": 0}]
+    "space-before-function-paren": {"anonymous": 2, "named": 0, "asyncArrow": 0}
   }
 }
