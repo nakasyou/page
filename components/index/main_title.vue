3a6dc475cbffee980692fa9c487ad9d6bab0241a
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-18 17:13:22 +0900

    delete config

--- components/index/main_title.vue
+++ components/index/main_title.vue
@@ -20,18 +20,5 @@
 <script>
 export default {
   name: "MainTitle",
-  data() {
-    return {
-      scrollY: 0
-    }
-  },
-  mounted() {
-    window.addEventListener('scroll', this.handleScroll)
-  },
-  methods: {
-    handleScroll() {
-      this.scrollY = window.scrollY
-    }
-  }
 }
 </script>
