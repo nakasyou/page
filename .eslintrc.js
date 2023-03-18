99492f1cc9fb94ec1d8accda2b97d12538ab9804
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-18 17:11:15 +0900

    フォーマット

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,10 +19,10 @@ module.exports = {
     "comma-dangle": ["off","never"],
     "comma-spacing": ["off"],
     "eol-last": ["off"],
-    "space-before-function-paren":["error", {
-      "anonymous": "always",
-      "named": "never",
-      "asyncArrow": "always"
+    "space-before-function-paren": ["error", {
+      anonymous: "always",
+      named: "never",
+      asyncArrow: "always",
     }],
   }
 }
