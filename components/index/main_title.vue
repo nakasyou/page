d95ae35d9601c04315233c12abb22246849cf6b8
nakasyou <nakasyou1103@gmail.com>
2023-03-23 18:57:10 +0900

    add スクロール

--- components/index/main_title.vue
+++ components/index/main_title.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="main-title">
+  <div class="main-title" ref="mainTitle">
     <img class="liberluna-title-image"
       src="@/assets/liberluna/liberluna-logo.svg"/>
     <img class="scroll-to-down"
-      src="@/assets/icons/scroll.svg"/>
+      src="@/assets/icons/scroll.svg"
+      @click="scrollToDown"/>
   </div>
 </template>
 <style>
@@ -14,15 +15,30 @@
   position:relative;
 }
 .liberluna-title-image{
-  width:90%;
+  width:50%;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translateY(-50%) translateX(-50%);
+  min-width: 50px;
+}
+.scroll-to-down{
+  transform: translateX(-50%);
+  position: absolute;
+  top: 90%;
+  left: 50%;
 }
 </style>
 <script>
 export default {
   name: "MainTitle",
+  methods: {
+    scrollToDown(){
+      window.scrollTo({
+        top: this.$refs.mainTitle.clientHeight,
+        behavior: 'smooth'
+      });
+    }
+  }
 }
 </script>
