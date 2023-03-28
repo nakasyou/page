ec452ebe1ecbcd106f5cddd7b38d394a9c3416df
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-28 14:50:33 +0900

    Update main_desc.vue

--- components/index/main_desc.vue
+++ components/index/main_desc.vue
@@ -33,7 +33,7 @@ export default {
   }
 }
 </script>
-<style>
+<style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0");
 .material-symbols-outlined{
   vertical-align: sub;
