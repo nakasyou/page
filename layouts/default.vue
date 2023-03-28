22388a4d9a0b23b35d96ad832ae0f72ac3295aa6
nakasyou <nakasyou1103@gmail.com>
2023-03-28 18:27:57 +0900

    fix icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,8 +7,12 @@ html,body, #__nuxt, #__layout{
   height:100%;
   width: 100%;
 }
-@import url("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0");
-.material-symbols-outlined{
+.material-symbols-outlined {
+  font-variation-settings:
+  'FILL' 0,
+  'wght' 400,
+  'GRAD' 0,
+  'opsz' 48;
   vertical-align: sub;
   font-size: 1em;
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" },
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" },
     ]
   },
 
