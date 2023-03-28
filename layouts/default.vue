4c6adeded763f106f816f05af776251634b032a0
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-28 14:51:08 +0900

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,4 +7,9 @@ html,body, #__nuxt, #__layout{
   height:100%;
   width: 100%;
 }
+@import url("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0");
+.material-symbols-outlined{
+  vertical-align: sub;
+  font-size: 1em;
+}
 </style>
