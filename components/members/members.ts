f31b8bd1090ce07a780d83ab901eeef815259c43
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2023-04-08 01:47:58 +0900

    Update members.ts

--- components/members/members.ts
+++ components/members/members.ts
@@ -340,7 +340,7 @@ const members = () => [
       ja: ""
     },
     posts: ["Developer"],
-    skills: ["JavaScript", "Python", "TypeScript", "PHP", "lua"],
+    skills: ["JavaScript","lua", "PHP",  "Python", "TypeScript"],
     icon: "https://github.com/Mambo-q.png",
     links: [
       {
