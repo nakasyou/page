4edfa05cb5b18c7c6b0de108b4467b3a42acce55
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-26 15:43:07 +0900

    Create what_does_liberluna.vue

---
+++ pages/what_does_liberluna.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="member-root">
+    <LiberlunaHeader/>
+    <h1>Liberlunaは何をしているの？</h1>
+    <div>
+      <div>
+        Liberlunaは、学生が多くいるコミュニティです。
+      </div>
+    </div>
+    <MembersFolder/>
+  </div>
+</template>
+<script>
+import Vue from "vue"
+import MembersFolder from "~/components/members/members_folder.vue"
+import LiberlunaHeader from '~/components/liberluna_header.vue'
+
+export default Vue.extend({
+  name: "MembersPage",
+  components: {
+    MembersFolder,
+    LiberlunaHeader,
+  }
+})
+</script>
+<style scoped>
+.member-root{
+  width: 100%;
+  height: 100%;
+}
+</style>
